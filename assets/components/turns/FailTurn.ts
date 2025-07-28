cbc0be17da5fdbac8a788f32bc20aeee0dbaa61f
khoa9894 <khicondangyeu490@gmail.com>
2025-07-28 22:52:46 +0700

    addManager

--- assets/components/turns/FailTurn.ts
+++ assets/components/turns/FailTurn.ts
@@ -1,9 +1,11 @@
-import { Turn } from '../../type/global'
+import { Popup, Turn } from '../../type/global'
 import { LevelLoader } from '../level/LevelLoader'
+import { UImanager } from '../ui-manager/UImanager'
 import { BaseTurn } from './BaseTurn'
 export class FailTurn extends BaseTurn {
     onEnter(): void {
         console.log('Fail')
+        UImanager.showPopup(Popup.FAILPOPUP, true)
     }
 
     onExit(): void {}
